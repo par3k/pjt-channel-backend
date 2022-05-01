89a7e4b372dd8e7e0314ec41d9a176c412c34b91
Hoijai Park (Alex) <hoijae0194@naver.com>
2022-05-01 11:11:32 +0900

    [22.05.01/박회재] Logging Aspect 추가

---
+++ src/main/java/com/zipsin/common/aop/LoggingAspect.java
@@ -0,0 +1,46 @@
+package com.zipsin.common.aop;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+
+@Aspect
+@Component
+public class LoggingAspect {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+	
+	@Pointcut("within(com.zipsin.*.controller..*)")
+	public void onRequest() { }
+	
+	@Around("com.zipsin.common.aop.LoggingAspect.onRequest()")
+	public Object requestLogging(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+		long start = System.currentTimeMillis();
+		try {
+			return proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
+		} finally {
+			long end = System.currentTimeMillis();
+			logger.info("Request : {} {} : {} ({}ms)", request.getMethod(), request.getRequestURI(), paramMapToString(request.getParameterMap()), end - start);
+		}
+	}
+	
+	private String paramMapToString(Map<String, String[]> paramStringMap) {
+		return paramStringMap.entrySet().stream()
+				.map(entry -> String.format("%s : %s", entry.getKey(), Arrays.toString(entry.getValue())))
+				.collect(Collectors.joining(", "));
+	}
+}

2cccfd3392db4e2cb0c08304c1817d84acf3f062
Hoijai Park (Alex) <hoijae0194@naver.com>
2022-05-01 11:08:10 +0900

    [22.05.01/박회재] 패키지 명 변경

--- src/main/java/com/zipsin/common/config/WebSecurityConfig.java
+++ src/main/java/com/zipsin/common/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.zipsin.common.utils;
+package com.zipsin.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
